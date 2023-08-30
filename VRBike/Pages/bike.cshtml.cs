fb53738eed5f808a6997d9518bd87ccc6025b297
daniele.paoloni <daniele.paoloni@dedagroup.it>
2023-08-31 01:11:33 +0200

    bike

---
+++ VRBike/Pages/bike.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace VRBike.Pages
+{
+    public class bikeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
